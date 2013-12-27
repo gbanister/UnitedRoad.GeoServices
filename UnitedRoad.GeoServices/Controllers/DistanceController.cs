60724ff7740cb5bc8d6df5dce02a654ec6f1de7b
Greg Banister <banister@icloud.com>
2013-12-27 17:03:53 -0600

    adding app settings file

--- UnitedRoad.GeoServices/Controllers/DistanceController.cs
+++ UnitedRoad.GeoServices/Controllers/DistanceController.cs
@@ -3,6 +3,8 @@
 	using System;
 	using System.IO;
 	using System.Net;
+	using System.Security.Cryptography;
+	using System.Text;
 	using System.Web.Http;
 	using Newtonsoft.Json;
 
@@ -13,6 +15,8 @@
 			var url = @"http://maps.googleapis.com/maps/api/distancematrix/json?origins=" +
 			          origin + "&destinations=" + destination +
 			          "&mode=driving&sensor=false&language=en-EN&units=imperial";
+
+			url += "&client=";
 
 			var request = (HttpWebRequest) WebRequest.Create(url);
 			var response = request.GetResponse();
@@ -34,5 +38,26 @@
 
 		}
 
+		public static string Sign(string url, string keyString)
+		{
+			var encoding = new ASCIIEncoding();
+
+			// converting key to bytes will throw an exception, need to replace '-' and '_' characters first.
+			var usablePrivateKey = keyString.Replace("-", "+").Replace("_", "/");
+			var privateKeyBytes = Convert.FromBase64String(usablePrivateKey);
+
+			var uri = new Uri(url);
+			var encodedPathAndQueryBytes = encoding.GetBytes(uri.LocalPath + uri.Query);
+
+			// compute the hash
+			var algorithm = new HMACSHA1(privateKeyBytes);
+			var hash = algorithm.ComputeHash(encodedPathAndQueryBytes);
+
+			// convert the bytes to string and make url-safe by replacing '+' and '/' characters
+			var signature = Convert.ToBase64String(hash).Replace("+", "-").Replace("/", "_");
+
+			// Add the signature to the existing URI.
+			return uri.Scheme + "://" + uri.Host + uri.LocalPath + uri.Query + "&signature=" + signature;
+		}
 	}
 }
